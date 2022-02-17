33d16c423747b3ed0df4f8174e2f189540737887
super0326 <superdev678@gmail.com>
2022-02-18 09:21:09 +1000

    Update main.js

--- js/main.js
+++ js/main.js
@@ -529,7 +529,7 @@ $(document).ready(function(){
 	};
 	setInterval(timer, 1000);
 		// Set the date we're counting down to
-		var countDownDate = new Date("feb 28, 2022 12:00:00").getTime();
+		var countDownDate = new Date("feb 22, 2022 12:00:00").getTime();
 		// Update the count down every 1 second
 		var x = setInterval(function() {
 		// Get todays date and time
